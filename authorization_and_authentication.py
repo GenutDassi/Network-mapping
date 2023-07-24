2d4a6e88411569200b45b91e0fc76962f4a40a9c
הדסה גנוט <dassigenut1234@gmail.com>
2023-07-24 16:59:01 +0300

    24/07/2023 start adding files project

---
+++ authorization_and_authentication.py
@@ -0,0 +1,11 @@
+def login(name, password):
+    # return token
+    pass
+
+
+def signup(name, password):
+    pass
+
+
+def check_permission(client):
+    pass
---
+++ network_in_db.py
@@ -0,0 +1,2 @@
+async def get_network_info(client, network_name):
+    pass
