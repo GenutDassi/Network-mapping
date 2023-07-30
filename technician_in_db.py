af4e7416793ce2fb7ae670b2db8eb32aa2bd9e00
DraymanR <dr0548424205@gmail.com>
2023-07-30 10:26:32 +0300

    30/07/2023 add func to technician_in_db.py

--- technician_in_db.py
+++ technician_in_db.py
@@ -6,16 +6,6 @@ async def add_technician(technician_name, technician_password):
     return await db_access.execute_query(query, (technician_name, technician_password))
 
 
-# async def update_technician(technician_id,):
-#     query = ''
-#     return db_access.execute_query(query)
-
-
-# async def get_details(technician_id):
-#     query = f'SELECT '
-#     return db_access.execute_query(query)
-
-
 async def is_authorized(technician_id, client_id):
     query = f'SELECT id FROM permission WHERE client_id = {client_id} AND technician_id = {technician_id}'
     authorized = await db_access.execute_query(query)
