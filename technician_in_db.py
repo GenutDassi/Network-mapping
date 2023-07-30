bcf01525accc611c754f2a028ded5d6c2bee31e4
DraymanR <dr0548424205@gmail.com>
2023-07-30 16:52:03 +0300

    30/07/2023 try commit to pydantic

---
+++ technician_in_db.py
@@ -0,0 +1,14 @@
+import db_access
+
+
+async def add_technician(technician_name, technician_password):
+    query = """INSERT IGNORE INTO technician (name, password) VALUES (%s, %s)"""
+    return await db_access.execute_query(query, (technician_name, technician_password))
+
+
+async def is_authorized(technician_id, client_id):
+    query = f'SELECT id FROM permission WHERE client_id = {client_id} AND technician_id = {technician_id}'
+    authorized = await db_access.execute_query(query)
+    if authorized:
+        return True
+    return False
