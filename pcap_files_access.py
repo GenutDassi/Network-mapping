c67f82845446c9e2ce8a4ccc86912afd52d39e8f
ChaviDaitsh <chaviDaitsh@gmail.com>
2023-07-30 11:48:48 +0300

    30/07/2023 Implement upload file in pcap_files_access

--- pcap_files_access.py
+++ pcap_files_access.py
@@ -1,5 +1,12 @@
-def upload_file(path):
-    pass
+from tkinter import filedialog
+from scapy.all import rdpcap
+
+def upload_file():
+    file_path = filedialog.askopenfilename(filetypes=[("PCAP files", "*.pcap")])
+    if file_path:
+        file = rdpcap(file_path)
+        return file
+    return None
 
 
 def read_file(path):
