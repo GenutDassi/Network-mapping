b3ff51ffb00d72b6b101f638073a33ac8699c832
DraymanR <dr0548424205@gmail.com>
2023-07-31 15:51:03 +0300

    31/07/2023 add func get_full_network()

--- network_in_db.py
+++ network_in_db.py
@@ -11,20 +11,20 @@ async def get_network_info(client_id, network_name):
 
 
 async def create_network(new_network):
-    await db_access.execute_query("INSERT IGNORE INTO network (client_id, name, location) VALUES (%s);", (new_network))
+    await db_access.execute_query("INSERT IGNORE INTO network (client_id, name, location) VALUES (%s);", new_network)
     new_network_id = await db_access.execute_query("SELECT @@IDENTITY AS [@@IDENTITY];")
     return new_network_id
 
 
 async def create_device(new_device):
-    await db_access.execute_query(f"INSERT IGNORE INTO device (ip, mac, network_id) VALUES (%s);", (new_device))
+    await db_access.execute_query(f"INSERT IGNORE INTO device (ip, mac, network_id) VALUES (%s);", new_device)
     new_device_id = await db_access.execute_query("SELECT @@IDENTITY AS [@@IDENTITY];")
     return new_device_id
 
 
 async def create_connection(new_connection):
     await db_access.execute_query("INSERT IGNORE INTO connection (src_device_id, dst_device_id, protocol) VALUES (%s);",
-                                  (new_connection))
+                                  new_connection)
     new_connection_id = await db_access.execute_query("SELECT @@IDENTITY AS [@@IDENTITY];")
     return new_connection_id
 
@@ -33,7 +33,7 @@ async def get_devices(client_id, network_name):
     network_id = await db_access.execute_query("SELECT network.id FROM network WHERE client_id=%s AND name=%s;",
                                                (client_id, network_name))
     return await db_access.execute_query(
-        "SELECT device.mac, device.ip, device.vendor FROM device WHERE network_id=%s;", (network_id))
+        "SELECT device.mac, device.ip, device.vendor FROM device WHERE network_id=%s;", network_id)
 
 
 # async def get_connections(client_id, network_name): network_id = await db_access.execute_query("SELECT network.id
